9f37d9022fedcd563d7a814bd45a2dee0d7d5c07
dragon <isanoguchi721@gmail.com>
2023-01-29 13:05:54 -0500

    fix

--- src/components/site/Site.tsx
+++ src/components/site/Site.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 const Site: React.FC<Props> = ({ data }) => {
   return (
-    <div className="bg-[#22222280] w-[280px] lg:w-[400px] rounded-[20px] mx-auto">
+    <div className="bg-[#22222280] w-[400px] md:w-[280px] lg:w-[400px] rounded-[20px] mx-auto">
       <img alt="img" src={data[0]} className="h-[20vh] rounded-t-[20px]" />
       <div className="flex py-4 justify-between px-4">
         <p className="font-[Sofia] text-white">{data[1]}</p>
