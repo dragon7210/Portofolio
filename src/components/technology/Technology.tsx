d51d97a00d612afd607079799c4c2b2a422a88f9
dragon <isanoguchi721@gmail.com>
2023-01-29 08:11:05 -0500

    fix

--- src/components/technology/Technology.tsx
+++ src/components/technology/Technology.tsx
@@ -4,13 +4,16 @@ const Technology = () => {
   return (
     <div className="mt-5 grid grid-cols-3 w-[70%]">
       {Techlists.map((list, index) => (
-        <div key={index} className="flex text-[#64ffda] items-center">
+        <div key={index} className="flex text-[#64ffda] items-center mt-3">
           <img
             src={list[1]}
-            className="w-4 h-4 mr-5 rounded-[50%]"
+            className="w-6 h-6 mr-5 rounded-[50%]"
             alt="techImg"
           />
-          <span className="text-[#8892b0] font-[Sofia]"> {list[0]}</span>
+          <span className="text-[#8892b0] font-[Sofia] text-[18px]">
+            {" "}
+            {list[0]}
+          </span>
         </div>
       ))}
     </div>
